908a4521a94df974e5a9a24b9b6930b97d7c81ea
Emily <yeouvi29@gmail.com>
2022-04-19 06:05:55 -0700

    feat: align Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
+import { COLORS, WEIGHTS } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
 
 const Header = () => {
   // Our site features two visual headers, but they should be
@@ -28,10 +28,20 @@ const Header = () => {
 
 const MainHeader = styled.div`
   padding: 0 32px;
+  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  column-gap: 48px;
+  margin: auto;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
